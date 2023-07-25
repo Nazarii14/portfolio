1cf8f5fa49190e9c7c4c6744b9b3496e5253f32c
Rosaka12 <92648981+Rosaka12@users.noreply.github.com>
2023-07-25 12:19:21 +0300

    added light mode switch and projects section responsiveness

--- js/app.js
+++ js/app.js
@@ -152,10 +152,48 @@ VanillaTilt.init(document.querySelector(".btn.btn2"), {
 const toTopButton = document.querySelector(".to-top");
 
 window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 100) {
+  if (window.scrollY > 100) {
     toTopButton.classList.add("active")
   }
   else {
     toTopButton.classList.remove("active")
   }
 })
+
+
+const toggle = document.querySelector('.switch input[type="checkbox"]');
+
+function toggleTheme() {
+  if (toggle.checked) {
+    updateCSSVariables("#00fbff", "#ffffff", "#000000", "#626262");
+
+    // Switch to dark theme
+  } else {
+    // Switch to light theme
+    updateCSSVariables("#00fbff", "#000000", "#ffffff", "#262626");
+
+  }
+}
+
+function updateCSSVariables(mainColor, blackColor, whiteColor, greyColor) {
+  document.documentElement.style.setProperty('--main-color', mainColor);
+  document.documentElement.style.setProperty('--black-color', blackColor);
+  document.documentElement.style.setProperty('--white-color', whiteColor);
+  document.documentElement.style.setProperty('--grey-color', greyColor);
+}
+
+toggle.addEventListener('change', toggleTheme);
+
+// let darkMode = localStorage.getItem("switch")
+// const darkModeToggle = document.querySelector("switch")
+
+// darkMode.addEventListener('click', function () {
+//   console.log('test');
+// })
+
+// const toggle = document.getElementsByClassName('switch')
+// const body = document.getElementById('body')
+
+// toggle.addEventListener('click', function () {
+//   this.classList.toggle()
+// })
